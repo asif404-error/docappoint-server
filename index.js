f6d2985e1b67222e0658d07659ae9f089ba88b80
PhantomOblivion <smasif404@gmail.com>
2026-05-21 14:55:01 +0600

    Add JWT token generation route for user authentication

--- index.js
+++ index.js
@@ -17,4 +17,24 @@ app.use(
 );
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log(token);
+  console.log(authHeader);
+  if (!token) {
+    return res
+      .status(401)
+      .send({ message: "Unauthorized - No token provided" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden - Invalid token" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 run();
